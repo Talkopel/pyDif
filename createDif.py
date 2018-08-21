2b1d2312b7112c5c5a3eadfe33b40aa0e76c9680
CompTeam <tal.kopel@outlook.com>
2018-08-21 13:09:58 +0300

    added angular js boilerplate

--- createDif.py
+++ createDif.py
@@ -1,23 +1,10 @@
 import struct
-import jinja
+import jinja2
 
 
 
 if __name__ == "__main__":
 
-	chunks = []
-
-	with open("dump1.dat", "rb") as f:
-		
-		nextInLine = 0
-
-		while nextInLine != -1:
-			
-			print 1
-
-			nextInLine = struct.unpack('i', f.read(4))[0]
-
-			chunks.append(f.read(nextInLine))
 
 
-	print chunks	
+	print "chunks"	
---
+++ static/main.js
@@ -0,0 +1,8 @@
+angular.module('pyDif', ['ngMaterial']);
+(() => {
+	angular.module('pyDif', ['ngMaterial']);
+
+	function pyDifCtrl($scope) {
+		$scope.bla = "sdfsdf";
+	}
+})();
